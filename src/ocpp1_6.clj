346f2c0771803ffeb84a61ec6151a1aa04bd17eb
Guilherme Augusto Gessele <gagessele@icloud.com>
2024-08-17 19:00:46 -0300

    specification for :authorization/status and :identifier/id-tag-info

--- src/ocpp1_6.clj
+++ src/ocpp1_6.clj
@@ -2,9 +2,13 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.string :as str]))
 
+(s/def :authorization/status #{:accepted :blocked :expired :invalid :concurrent-tx})
+
 (s/def :identifier/id-token (s/and string? (complement str/blank?) #(>= 20 (count %))))
 
-(s/valid? :identifier/id-token nil)
-(s/valid? :identifier/id-token "")
-(s/valid? :identifier/id-token "abcd")
-(s/valid? :identifier/id-token "aaaaaaaaaaaaaaaaaaaaa")
+(s/def :identifier/parent-id-tag :identifier/id-token)
+(s/def :identifier/expiry-date inst?)
+(s/def :identifier/id-tag-info
+  (s/keys :req-un [:authorization/status]
+          :opt-un [:identifier/expiry-date
+                   :identifier/parent-id-tag]))
